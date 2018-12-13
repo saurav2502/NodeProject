607f6a10007967e07d8a544dc815580148cac5bd
saurav2502 <sauravstarsk@gmail.com>
2018-12-13 20:59:41 +0530

    code added

--- routes/loginSuccess.js
+++ routes/loginSuccess.js
@@ -7,16 +7,18 @@ router.get('/', function(req, res, next) {
 });
 router.post('/', function(req, res, next) {
 
-    if (req.param('username')=="saurav"){
-        if (req.param('passwrd')=="kumar"){
-            res.render('loginSuccess', { title: 'Express' });
-        }
-        else
-            res.send("password wrong");
+    if (req.param('username')!=null && req.param('username')!="" && req.param('passwrd')!="" && req.param('passwrd')!=null) {
 
+        if (req.param('username') == "saurav") {
+            if (req.param('passwrd') == "kumar") {
+                res.render('loginSuccess', {title: 'Express'});
+            } else
+                res.send("password wrong");
+        } else
+            res.send("Userid did not match");
     }
     else
-        res.send("Userid did not match");
+        res.send("Username or password is empty");
 
     //res.render('loginSuccess', { title: 'Express' });
 });
